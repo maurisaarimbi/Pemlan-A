4e4fdec213c8115ddb5818243dbb16eeca0c429e
maurisaarimbi <caca.maurisa@gmail.com>
2020-09-23 10:31:56 +0700

    first commit

---
+++ main Biodata.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	
+	printf("Nama\t\t\t : Maurisa Arimbi Putri\n");
+	printf("NPM\t\t\t : 19081010192\n");
+	printf("Tempat, tanggal lahir\t : Sidoarjo, 4 Juni 2001\n");
+	printf("Alamat\t\t\t : Permata Megah Asri D-10, Sidokepung, Buduran, Sidoarjo\n");
+	printf("E-mail\t\t\t : caca.maurisa@gmail.com\n");
+	printf("No. HP\t\t\t : 082139240985\n");
+	printf("Jenis Kelamin\t\t : Perempuan\n");
+	printf("Agama\t\t\t : Islam\n");
+	printf("Kewarganegaraan\t\t : WNI\n");
+	
+	return 0;
+}
